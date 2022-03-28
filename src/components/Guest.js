abe7267a87a3cea7c30b19d91c8f09a33c055b0c
bloodredhood <3drusher@gmail.com>
2022-03-28 18:33:25 +0300

    add formik and yup

--- src/components/Guest.js
+++ src/components/Guest.js
@@ -1,30 +1,89 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import "./../App.css"
 import { Rate } from "antd";
+import { Field, useFormik } from "formik"
+import * as Yup from "yup"
 
 //value={props.commonState[guestIdx].formInfo.rating}
 
 const Guest = (props) => {
   const { guestIdx } = useParams()
-  console.log(props.commonState[guestIdx])
+  const formik = useFormik({
+    inintialValues: {
+      rating: 0,
+      phone: "",
+      comment: ""
+    },
+    validationSchema: Yup.object({
+      rating: Yup.number().required("Required"),
+      phone: Yup.string(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[\s/0-9]*$/,
+      "incorrect phone number format").min(3, "min length is 3").max(10, "max length is 10"
+      ).required("Required"),
+      comment: Yup.string()
+    }),
+    onSubmit: (values) => {
+      //dispatch to store
+    }
+  })
+  //console.log(props.commonState[guestIdx])
   return (
-    <form className="forma">
-      <label>NAME</label>
-      <div className="formTextGray">{props.commonState[guestIdx].name}</div>
-      <Rate />
-      <div>
-        <label>PHONE</label>
-      </div>
-      <input />
-      <div>
-        <label>COMMENT</label>
-      </div>
-      <textarea></textarea>
-      <div>
-        <button className="formButton">SAVE</button>
-      </div>
-    </form>
+    <>
+      {
+        props.commonState[guestIdx].formInfo.rating === 0
+          ? < form className="forma" onSubmit={formik.handleSubmit}>
+            <label>NAME</label>
+            <div className="formTextGray">{props.commonState[guestIdx].name}</div>
+            <Rate
+              value={formik.values.rating}
+              onChange={formik.handleChange}
+            />
+            <div>
+              <label htmlFor="phone">PHONE</label>
+            </div>
+            <Field
+              as="input"
+              id="phone"
+              name="phone"
+              type="text"
+              placeholder="phone"
+              onChange={formik.handleChange}
+              value={formik.values.phone}
+            />
+            <div>
+              <label htmlFor="comment">COMMENT</label>
+            </div>
+            <Field
+              as="textarea"
+              id="comment"
+              name="comment"
+              placeholder="comment"
+              onChange={formik.handleChange}
+              value={formik.values.comment}
+            />
+            <div>
+              <button className="formButton">SAVE</button>
+            </div>
+          </form>
+          : <div className="forma">
+            <label>NAME</label>
+            <div className="formTextGray">{props.commonState[guestIdx].name}</div>
+            <Rate value={props.commonState[guestIdx].formInfo.rating} />
+            <div>
+              <label>PHONE</label>
+            </div>
+            <div className="formTextGray">
+              {props.commonState[guestIdx].phone}
+            </div>
+            <div>
+              <label>COMMENT</label>
+            </div>
+            <div className="formTextGray">
+              {props.commonState[guestIdx].comment}
+            </div>
+          </div>
+      }
+    </>
   )
 }
 
